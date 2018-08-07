7433980c07b3de418f7f07cb6a6fe7a1035bcf90
Pedro Grisotti <pedro@dzigual.com.br>
2018-08-07 09:24:15 -0300

    Adicionando Sass

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,18 @@ const path = require("path");
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
+
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS
+        ]
+    }]
+  },
+
   devServer: {
     contentBase: path.join(__dirname, "public"),
     port: 3210,
